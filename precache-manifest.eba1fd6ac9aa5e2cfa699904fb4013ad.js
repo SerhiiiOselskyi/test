e11d9a3223d4deda2c636ce9780b2dbb6f06bbde
bran4u <s.oselskiy@gmail.com>
2020-08-24 23:07:05 +0300

    gh-pages

---
+++ precache-manifest.eba1fd6ac9aa5e2cfa699904fb4013ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1411c00bb7c9adcd36d223882508ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f9a5af357f47de5f5cd0",
+    "url": "./static/css/main.a5e8742a.chunk.css"
+  },
+  {
+    "revision": "c8555e581597bd41cd04",
+    "url": "./static/js/2.3c6003f5.chunk.js"
+  },
+  {
+    "revision": "35a92c1da7372c73f674ebb80274fbd5",
+    "url": "./static/js/2.3c6003f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9a5af357f47de5f5cd0",
+    "url": "./static/js/main.d828adcc.chunk.js"
+  },
+  {
+    "revision": "717f2e3de498d07973e0",
+    "url": "./static/js/runtime-main.c245507d.js"
+  }
+]);
